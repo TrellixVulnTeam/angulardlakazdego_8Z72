a6366256a0d7ee7f66062b3bd8fed66747e41402
tomaszsobkow <sobeczki@gmail.com>
2022-04-20 18:10:50 +0100

    working with import one class to another class

---
+++ src/app/introduction/modules/imports.ts
@@ -0,0 +1,9 @@
+import { One } from "./models";
+import * as models from './models';
+
+let one = new One();
+let two = new models.Two();
+let PI = models.myPI;
+
+console.log(PI);
+
---
+++ src/app/introduction/modules/models.ts
@@ -0,0 +1,16 @@
+export class One{
+
+    constructor(){
+        console.log('Class ONE');
+    }
+}
+
+export class Two{
+
+    constructor(){
+        console.log('Class ONE');
+    }
+}
+
+export const myPI = 3.14;
+ 
