1b8330273da1187cb54b2027ece258267eb8072f
Tomasz Sobkow <Tomasz.Sobkow@gmail.com>
2022-07-07 11:05:26 +0100

    send parent array to child1

--- src/app/tests/communication/communication.component.ts
+++ src/app/tests/communication/communication.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CommunicationComponent implements OnInit {
 
-  title = 'Communication between components';
+  title = 'Skills Communication between components';
   animalsParent: any[] = ['Dog','Cat','Bird','Fish']
 
 
